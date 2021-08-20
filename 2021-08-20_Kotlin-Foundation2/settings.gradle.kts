e9cc38f6d1e91a890de612a6a8e7b8364249ae76
aahspringaa4 <aahspringaa4@naver.com>
2021-08-20 16:38:36 +0900

    [ADD] Kotlin 기초 변수 선언

---
+++ 2021-08-20_Kotlin-Foundation2/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "2021-08-20_Kotlin-Foundation2"
+
---
+++ 2021-08-20_Kotlin-Foundation2/src/Kotlin_Study.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val number = 10;
+    var language = "korean"
+    val secondBumber: Int = 20
+    language = 20 // var로 한번 할당한 값은 바꿀 수 없음
+
+    println("number: $number")
+    println("language: $language")
+    println("secomdNumber: $secondBumber")
+}
